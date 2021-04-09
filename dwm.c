8cf6a3b57b7a9047ca1e5a90eb4cc1b922b1b714
tony <tony.aldon.adm@gmail.com>
2021-04-09 09:44:47 +0200

    add layout (centeredfloatingmasteralways)
    * dwm.c:  Add layout (centeredfloatingmasteralways).  It's a small
      variation on the layout centeredfloatingmaster that forces the master
      area to be ALWAYS centered even if there is no clients in the stacking
      area.

--- dwm.c
+++ dwm.c
@@ -2255,3 +2255,44 @@ centeredfloatingmaster(Monitor *m)
 		tx += WIDTH(c);
 	}
 }
+
+void
+centeredfloatingmasteralways(Monitor *m)
+{
+	/* It's a small variation on the layout centeredfloatingmaster that */
+  /* forces the master area to be ALWAYS centered even if there is no */
+	/* clients in the stacking area. */
+
+	unsigned int i, n, w, mh, mw, mx, mxo, my, myo, tx;
+	Client *c;
+
+	/* count number of clients in the selected monitor */
+	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+
+	/* initialize nmaster area */
+	if (m->ww > m->wh) {
+		mw = m->nmaster ? m->ww * m->mfact : 0;
+		mh = m->nmaster ? m->wh * 0.9 : 0;
+	} else {
+		mh = m->nmaster ? m->wh * m->mfact : 0;
+		mw = m->nmaster ? m->ww * 0.9 : 0;
+	}
+	mx = mxo = (m->ww - mw) / 2;
+	my = myo = (m->wh - mh) / 2;
+
+	for(i = tx = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
+	if (i < m->nmaster) {
+		/* nmaster clients are stacked horizontally, in the center
+		 * of the screen */
+		w = (mw + mxo - mx) / (MIN(n, m->nmaster) - i);
+		resize(c, m->wx + mx, m->wy + my, w - (2*c->bw),
+		       mh - (2*c->bw), 0);
+		mx += WIDTH(c);
+	} else {
+		/* stack clients are stacked horizontally */
+		w = (m->ww - tx) / (n - i);
+		resize(c, m->wx + tx, m->wy, w - (2*c->bw),
+		       m->wh - (2*c->bw), 0);
+		tx += WIDTH(c);
+	}
+}
