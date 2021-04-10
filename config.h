0be3b5ff36081666c810d720e8ba78d64ae41695
tony <tony.aldon.adm@gmail.com>
2021-04-10 09:53:36 +0200

    use only 2 tags
    * config.h:  Use only 2 tags.

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2" };
 
 static const Rule rules[] = {
   /* xprop(1):
@@ -27,8 +27,6 @@ static const Rule rules[] = {
    *  WM_NAME(STRING) = title
    */
   /* class      instance    title       tags mask     isfloating   monitor */
-  { "Gimp",     NULL,       NULL,       0,            1,           -1 },
-  { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
   { "Chromium-browser",  NULL,       NULL,       2,       0,           -1 },
 };
 
