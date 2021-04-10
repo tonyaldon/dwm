fe52706341a92a895a6a475e063f99ab8e30320e
tony <tony.aldon.adm@gmail.com>
2021-04-10 10:46:48 +0200

    update colors of windows and (dmenucmd)
    * config.h:  Update colors of windows and (dmenucmd).

--- config.h
+++ config.h
@@ -11,11 +11,12 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_green1[]      = "#458b74";
+static const char col_green2[]      = "#316453";
 static const char *colors[][3]      = {
   /*               fg         bg         border   */
   [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-  [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+  [SchemeSel]  = { col_gray4, col_green2,  col_green1  },
 };
 
 /* tagging */
@@ -59,7 +60,7 @@ static const Layout layouts[] = {
 #define SHCMD_INTERACTIVE(cmd) { .v = (const char*[]){ "/bin/bash", "-ic", cmd, NULL } }
 
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green2, "-sf", col_gray4, NULL };
 
 static const char *termcmd[]  = { "st", NULL };
 
